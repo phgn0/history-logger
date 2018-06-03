cd5bbf557e55da79524fceeb93e86c616b9e16d9
Peter Hagen <phgn@protonmail.ch>
2018-06-03 15:40:21 +0200

    Add background TabManager initialization

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,6 @@
 // import save from "./save_data";
 // import TabManager from "./data";
-import TabManager from "./TabManager";
-const tabManager = new TabManager([]);
+import TabManager, { TabImportData } from "./TabManager";
 
 declare const browser: any; // the browser object
 
@@ -18,36 +17,78 @@ interface TabObject {
     openerTabId?: number;
 }
 
+let _tabManager: TabManager;
+
+// get all open tabs
+const tabManagerInit = browser.tabs.query({}).then((tabs: TabObject[]) => {
+    // console.log("all", tabs);
+
+    // convert tab data to our data format
+    const tabData: TabImportData[] = tabs.map((tab: TabObject) => {
+        return {
+            tabId: tab.id,
+            createdBy: tab.openerTabId || null,
+            index: tab.index,
+            page: {
+                url: tab.url,
+                title: tab.title,
+                favIconUrl: tab.favIconUrl
+            }
+        };
+    });
+
+    // initialize tab manager with imported tabs
+    _tabManager = new TabManager(tabData);
+
+    return Promise.resolve(_tabManager);
+});
+
+// tab manager need init time, so has to be requested async
+function getTabManager(): Promise<TabManager> {
+    if (_tabManager) {
+        // already initialized
+        return Promise.resolve(_tabManager);
+    } else {
+        // currently initializing
+        return tabManagerInit;
+    }
+}
+
 browser.tabs.onActivated.addListener(({ tabId, windowId }) => {
     console.log("active tab: ", tabId);
 });
 
 browser.tabs.onCreated.addListener((tab: TabObject) => {
     // console.log("created: ", tab.url);
-    tabManager.createTab(
-        tab.id,
-        tab.openerTabId ? tab.openerTabId : null,
-        tab.index
-    );
+    getTabManager().then(tabManager => {
+        tabManager.createTab(
+            tab.id,
+            tab.openerTabId ? tab.openerTabId : null,
+            tab.index
+        );
+    });
 });
 
 browser.tabs.onRemoved.addListener((tabId: number) => {
-    tabManager.closeTab(tabId);
+    getTabManager().then(tabManager => {
+        tabManager.closeTab(tabId);
+    });
 });
 
 // notify when we visit a new url
 browser.tabs.onUpdated.addListener(
     (tabId: number, changeInfo: { status: string }, tab: TabObject) => {
         // we cant use the onUpdated filter method, because compatibility is bad
-        // so filter here
-        // console.log(tabId, changeInfo);
+        // so filter here for complete page load
         if (changeInfo.status === "complete") {
             // we completed loading a page
             // console.log("updated: " + tabId + " ", tab.url, tab.openerTabId);
-            tabManager.navigateTo(tabId, {
-                url: tab.url,
-                title: tab.title,
-                favIconUrl: tab.favIconUrl
+            getTabManager().then(tabManager => {
+                tabManager.navigateTo(tabId, {
+                    url: tab.url,
+                    title: tab.title,
+                    favIconUrl: tab.favIconUrl
+                });
             });
         }
     }
