cbbc1e41df434a9d9f4893237ce3fbdde5b14a72
Peter Hagen <phgn@protonmail.ch>
2019-02-03 21:08:28 +0100

    Commit icons & add webpack static file copy

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        background: "./src/background.ts"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name]-bundle.js"
-    }
+  mode: "development",
+  entry: {
+    background: "./src/background.ts"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-bundle.js"
+  },
+  plugins: [
+    new CopyWebpackPlugin(
+      [
+        { from: "static", to: "." } // to is relative to outpath
+      ],
+      {}
+    )
+  ]
 };
